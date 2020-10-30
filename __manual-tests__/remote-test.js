70c66ebcb08fcf7882fc22b65407ca4d75d6f40d
Rubén <hey@rubenvara.io>
2020-10-30 13:35:16 +0100

    remote tests 🧪

--- __manual-tests__/remote-test.js
+++ __manual-tests__/remote-test.js
@@ -1,12 +1,19 @@
 const axios = require(`axios`);
-const { SCREENSHOT_ENDPOINT } = process.env;
-const url = process.env.SITE_URL;
+
+const event = {
+  body: JSON.stringify({
+    // config options
+    url: process.env.SITE_URL,
+    cloudinaryFolder: 'vpnf/screenshots',
+    fileName: 'F009',
+  }),
+};
 
 const start = async () => {
   let screenshotResponse;
   try {
-    screenshotResponse = await axios.post(SCREENSHOT_ENDPOINT, { url });
-    const { status, data /* , headers */ } = screenshotResponse;
+    screenshotResponse = await axios.post(process.env.LAMBDA_ENDPOINT, event);
+    const { status, data } = screenshotResponse;
     console.log(status);
     console.log(data);
   } catch (error) {
