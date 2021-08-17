1ed42d47a4505b5fea30ac7e82febcbcb4d93438
Rubén <hey@rubenvara.io>
2021-08-17 14:53:13 +0200

    cleanup

--- __manual-tests__/config.js
+++ __manual-tests__/config.js
@@ -1,9 +1,19 @@
+// edit this file to change the test settings
+// allowed config options:
+
+// - cloudinaryFolder
+// - fileName
+// - url
+// - width
+// - height
+// - fullPage
+
 exports.config = {
-  // config options
-  url: process.env.SITE_URL,
   // TODO: allow to choose folder in cloudinary
-  cloudinaryFolder: 'vpnf/screenshots',
+  // cloudinaryFolder: 'vpnf/screenshots',
+  cloudinaryFolder: 'test-screenshots',
   // hardcoded variable file name
   // TODO: allow to choose filename prefix, and auto-add timestamp to avoid duplicates
   fileName: `screenshot-${Date.now()}`,
+  url: process.env.SITE_URL,
 };
